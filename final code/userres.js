d917fbf6c5185564bfa8b7b8d7fb49d73e795000
scottmason13 <76920812+scottmason13@users.noreply.github.com>
2021-03-08 19:13:54 -0600

    March 8 712p

--- final code/userres.js
+++ final code/userres.js
@@ -1,3 +1,25 @@
 // Welcome to your reservations [Insert name], Each piece of equipment reserved with its picture and what gym its from and res start and end date
 // Back to home page button, log out button (can we redirect this back to the home page or do we need a 2nd button)
 // Pulling from database: based on userID need to pull ReservationID which has all items listed above
+
+let db = firebase.firestore()
+
+window.addEventListener('DOMContentLoaded', async function(event) {
+
+    document.querySelector('form').addEventListener('submit', async function(event) {
+    event.preventDefault()
+    
+    let renterName = document.querySelector('#name').value
+    let resStartDate = document.querySelector('#reservationstart').value
+    let resEndDate = document.querySelector('#reservationend').value
+
+    if (renterName.length && resStartDate.length && resEndDate.length > 0) {
+        let docRef = await db.collection('Reservations').add({
+          renterName: renterName,
+          resStartDate: resStartDate,
+          resEndDate: resEndDate
+        })
+      }
+
+    })
+})
