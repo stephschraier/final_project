9d52c1e6506b7c9444049c51406a21b521aca2fb
elliecarrow <ellie.carrow@kellogg.northwestern.edu>
2021-03-14 16:02:12 -0500

    backend2

--- netlify/functions/userAPI.js
+++ netlify/functions/userAPI.js
@@ -2,13 +2,27 @@ let firebase = require('./firebase')
 
 exports.handler = async function(event) {
   console.log('hello from the back-end!')
-  let data = [] // sample only...
+
+  let userData = [] 
+  
   let db = firebase.firestore()
   let querySnapshot = await db.collection('Users').get()
   console.log(`Number of Users: ${querySnapshot.size}`)
 
+  let users = querySnapshot.docs
+  for (let i = 0; i < users.length; i++){
+    let userID = users[i].id
+    let user = users[i].data()
+    //console.log(user)
+
+    userData.push({
+      id: userID,
+      text: user.text
+    })
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify(data)
+    body: JSON.stringify(userData)
   }
 }
