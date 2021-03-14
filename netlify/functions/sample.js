6b46ca581cfa224a7ca68183983d07a5c56555c9
elliecarrow <ellie.carrow@kellogg.northwestern.edu>
2021-03-14 15:41:31 -0500

    backend

--- netlify/functions/sample.js
+++ netlify/functions/sample.js
@@ -1,7 +1,12 @@
 let firebase = require('./firebase')
 
 exports.handler = async function(event) {
+  console.log('hello from the back-end!')
   let data = [] // sample only...
+  let db = firebase.firestore()
+  let querySnapshot = await db.collection('Users').get()
+  console.log(`Number of Users: ${querySnapshot.size}`)
+
   return {
     statusCode: 200,
     body: JSON.stringify(data)
