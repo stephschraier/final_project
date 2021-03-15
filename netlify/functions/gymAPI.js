2269e181a4bacbb1263a10de9ace5a6978bbc788
elliecarrow <ellie.carrow@kellogg.northwestern.edu>
2021-03-14 21:26:45 -0500

    additionalAPI

--- netlify/functions/gymAPI.js
+++ netlify/functions/gymAPI.js
@@ -5,28 +5,28 @@ exports.handler = async function(event) {
   console.log(event)
   let queryStringUserId = event.queryStringParameters.userId
 
-  let reservationsData = []
+  let gymssData = []
 
   let db = firebase.firestore()
-  let querySnapshot = await db.collection('reservations3')
+  let querySnapshot = await db.collection('Gym')
                               .where('userId', '==', queryStringUserId)
                               .get()
-  console.log(`Number to reservations in collection: ${querySnapshot.size}`)
+  console.log(`Number of gyms in collection: ${querySnapshot.size}`)
   
   
-  let reservations = querySnapshot.docs
+  let gyms = querySnapshot.docs
  //   console.log(user)
 // // loop through the post documents
-  for (let i=0; i<reservations.length; i++) {
-    let reservationId = reservations[i].id                                // the ID for the given post
-    let reservationData = reservations[i].data()                          // the rest of the post data
-    let equipmentQuery = await db.collection('equipment').doc(reservationsData.EquipmentID)         // likes from Firestore
+  for (let i=0; i<gyms.length; i++) {
+    let gymId = gyms[i].id                                // the ID for the given post
+    let gymData = gyms[i].data()                          // the rest of the post data
+    // let equipmentQuery = await db.collection('equipment').doc(reservationsData.EquipmentID)         // likes from Firestore
                              .get()
 // might need to do equipmentQuery.data()
 // EX: equipmentname: equipmentQuery.data().Equipment
 
 //     // add a new Object of our own creation to the reservationsData Array
-    reservationsData.push({
+    gymsData.push({
       id: reservationId,                                          
       imageUrl: reservationData.ImageUrl,  // if lives in reservations
       equipmentname: equipmentQuery.Equipment, //if doesn't live in reservations pull from Query                                                   
@@ -35,6 +35,6 @@ exports.handler = async function(event) {
 
   return {
     statusCode: 200,
-    body: JSON.stringify(reservationsData)
+    body: JSON.stringify(gymsData)
   }
 }
